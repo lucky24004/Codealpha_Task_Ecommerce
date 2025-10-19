c32a755442013a053a1a9771414486ed73a58868
lucky24004 <gopalalakshmiprasad@gmail.com>
2025-10-19 12:08:46 +0530

    Update settings for production environment
    Changed DEBUG setting to False and updated ALLOWED_HOSTS.

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-49&n$ip16%oz_+f)%akdce056v8a@p!r2bl^#mnzzkfm29m!^p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -148,6 +148,7 @@ LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'product_list'
 LOGOUT_REDIRECT_URL = 'login'
 LOGOUT_REDIRECT_URL = 'login'
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 
 
 
